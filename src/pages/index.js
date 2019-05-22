2c75f6ad652779ba7c864c20bbbe944de54e8b79
Andrew Horn <arhorn@smcm.edu>
2019-05-22 13:33:53 -0500

    updated index.js for meeting-sign-in app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/Layout/Layout'
-import SEO from '../components/Seo/Seo'
-import Form from '../components/TheForm/TheForm'
+import Layout from '../components/Layout/Layout';
+import SEO from '../components/Seo/Seo';
+import Form from '../components/TheForm/TheForm';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <p id="description">This is a simple survey form site where I'm testing out Gatsby and Formik.</p>
+    <p id="description">Welcome! Please provide your information. Thanks!</p>
     <Form />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
