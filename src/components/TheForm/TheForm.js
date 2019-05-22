975647f5d95336361b65acda495d6e1fb8a7ce8b
Andrew Horn <arhorn@smcm.edu>
2019-05-22 13:34:29 -0500

    Kadampa now has form info for name, email, how you heard of us, and first time

--- src/components/TheForm/TheForm.js
+++ src/components/TheForm/TheForm.js
@@ -4,99 +4,62 @@ import * as yup from 'yup';
 
 const RawForm = ({ values, errors, touched, isSubmitting }) => (
   <Form>
-    <label id="name-label">Name:</label>
+    <label>Name:</label>
     <div>
-      <Field
-        type="text"
-        name="name"
-        id="name"
-        placeholder="enter your name, thanks :)"
-      />
-      <ErrorMessage name="name" component="div" />
+      <Field type="text" name="name" id="name" />
+      <ErrorMessage name="name" component="p" />
     </div>
     <br />
-    <label id="email-label">Email:</label>
+    <label>Email:</label>
     <div>
-      <Field
-        type="email"
-        name="email"
-        id="email"
-        placeholder="enter your email, thanks :)"
-      />
-      <ErrorMessage name="email" component="div" />
-    </div>
-    <br />
-    <label id="number-label">Number:</label>
-    <div>
-      <Field
-        type="number"
-        name="number"
-        id="number"
-        placeholder="enter some number"
-      />
-      <ErrorMessage name="number" component="div" />
+      <Field type="email" name="email" id="email" />
+      <ErrorMessage name="email" component="p" />
     </div>
     <br />
     <div>
-      <Field component="select" name="dropdown" id="dropdown">
-        <option value="">please select one</option>
-        <option value="i like dropdowns">i like dropdowns</option>
-        <option value="i do not like dropdowns">i do not like dropdowns</option>
+      <Field component="select" name="heardOfUs" id="heardOfUs">
+        <option value="">How did you heard of us?</option>
+        <option value="friend">friend</option>
+        <option value="website">website</option>
+        <option value="meetup.com">meetup.com</option>
+        <option value="other">other</option>
       </Field>
-      <ErrorMessage name="dropdown" component="div" />
-    </div>
-    <br/>
-    <div name="radio-buttons-group">
-    <div>
-    <label>
-    <Field type="radio" name="radio" value="i can code in react" checked={values.radio === 'i can code in react'}/> 
-    i can code in react
-    </label>
-    </div>
-    <div>
-    <label>
-    <Field type="radio" name="radio" value="i cannot code in react" checked={values.radio === 'i cannot code in react'} /> 
-    i cannot code in react
-    </label>
-    </div>
-    <div>
-    <label>
-    <Field type="radio" name="radio" value="i am also gene roddenberry's cousin" checked={values.radio === 'i am also gene roddenberry\'s cousin'} /> 
-    i am also gene roddenberry's cousin
-    </label>
-    </div>
+      <ErrorMessage name="heardOfUs" component="p" />
     </div>
     <br />
+    {values.heardOfUs === 'other' && 
     <div>
-      <label>
-        <Field type="checkbox" name="iceCream" checked={values.iceCream} />i
-        like ice cream!
-      </label>
-    </div>
-
-    <div>
-      <label>
-        <Field
-          type="checkbox"
-          name="cottonCandy"
-          checked={values.cottonCandy}
-        />
-        i like cotton candy!
-      </label>
-    </div>
-
-    <div>
-      <label>
-        <Field type="checkbox" name="cake" checked={values.cake} />i like cake!
-      </label>
-    </div>
-    <br />
-    <label>
-      additional words here:
-      </label>
-      <div>
-      <textarea name="additionalComments" />
+        <label>Tell us more about how you heard of us:</label>
+        <br />
+        <Field type="text" name="heardOfUsDetails" id="heardOfUsDetails" />
       </div>
+    }
+    <br />
+    <label>First time here?</label>
+    <div name="first time">
+      <div>
+        <label>
+          <Field
+            type="radio"
+            name="first-time"
+            value="yes"
+            checked={values.firstTime === true}
+          />
+          yes
+        </label>
+      </div>
+      <div>
+        <label>
+          <Field
+            type="radio"
+            name="radio"
+            value="no"
+            checked={values.firstTime === false}
+          />
+          no
+        </label>
+      </div>
+    </div>
     <br />
     <div>
       <button type="submit" id="submit" disabled={isSubmitting}>
@@ -110,24 +73,14 @@ const TheForm = withFormik({
   mapPropsToValues({
     name,
     email,
-    number,
-    dropdown,
-    radio,
-    iceCream,
-    cottonCandy,
-    cake,
-    additionalComments
+    heardOfUs,
+    firstTime,
   }) {
     return {
       name: name || '',
       email: email || '',
-      number: number || '',
-      dropdown: dropdown || '',
-      radio: radio || 'i can code in react',
-      iceCream: iceCream || false,
-      cottonCandy: cottonCandy || false,
-      cake: cake || false,
-      additionalComments: additionalComments || ''
+      heardOfUs: heardOfUs || '',
+      firstTime: firstTime || true,
     };
   },
   validationSchema: yup.object().shape({
@@ -136,11 +89,7 @@ const TheForm = withFormik({
       .string()
       .email('email not valid')
       .required('email is required'),
-    number: yup
-      .number()
-      .min(2, 'number must be more than 2')
-      .max(8, 'number must be less than 8'),
-    dropdown: yup.string().required('you must make a choice')
+    heardOfUs: yup.string().required('we would like to know :)')
   }),
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
     setTimeout(() => {
